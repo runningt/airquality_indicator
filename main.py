6367107da61eef0530ddaeaab2bc9918b4a22a16
Piotr Zawislak <running.t@gmail.com>
2019-06-18 00:24:20 +0200

    First methods in main

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 import sys
+from indicator import Indicator, DEFAULT_STATION_ID
+
 
 def main():
-    print('test')
+    stationId = sys.argv[1] if len(sys.argv) > 1 else DEFAULT_STATION_ID
+    ind = Indicator(stationId)
+    print(ind.get_aqindex_url())
+    print(ind.get_details_url())
     return 0
 
 if __name__ == '__main__':
